d42587c8d968306949aceb64dc807f1fe6823db5
sourcecode123 <amuttur123@gmail.com>
2013-08-29 18:48:26 +0530

    renamed views. removed old comments

--- solve/urls.py
+++ solve/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('',
 	url(r'^display/$', views.display_sudoku, name='display'),
 	url(r'^solution/$', views.solve_sudoku, name='solution'),
 	url(r'^nosolution/$', views.no_solution, name='no_solution'),
-    url(r'^test/$', views.test, name='test'),
 )
--- solve/views.py
+++ solve/views.py
@@ -5,52 +5,37 @@ from django.views import generic
 from solution import Solution
 from django.core.context_processors import csrf
 
-temp_context =  {
-                    'grid' : Solution().returnGrid(),
-                    'nothing':[]
-                }
 def input_sudoku(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('solve/input.html', c)
-
-# Want to use the solve_sudoku view to receive post call with input grid and then have it
-# redirected (HttpResponseRedirect) to the display_sudoku view (since its a good practice)
-# But can't send context variables through HttpResponseRedirect so can't send the solved
-# drid to the sudoku grid to the display_sudoku view.
-# NOT sure how to go about it
-def solve_sudoku(request):
-    pass
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('solve/input.html', c)
 
 def no_solution(request):
-    return render(request, 'solve/unsolvable.html')
+	return render(request, 'solve/unsolvable.html')
+
+def solve_sudoku(request):
+	sudoku_input = []
+	for i in range(0, 81):
+		input_character = request.POST[str(i)]
+		if len(input_character) == 0:
+			sudoku_input.append('.')
+		else:
+			sudoku_input.append(input_character)
+
+	sudoku_solution = Solution(sudoku_input).returnGrid()
+	request.session['sudoku_solution'] = repr(sudoku_solution)
+
+	if not sudoku_solution:
+		return HttpResponseRedirect(reverse('solve:no_solution'))
+	else:
+		return HttpResponseRedirect(reverse('solve:display'))
 
 def display_sudoku(request):
-    sudoku_input = []
-    for i in range(0, 81):
-        input_character = request.POST[str(i)]
-        if len(input_character) == 0:
-            sudoku_input.append('.')
-        else:
-            sudoku_input.append(input_character)
+	sudoku_solution = request.session.get('sudoku_solution', None)
 
-    # returnGrid return a 'bool' if no solution or if function timesout.
-    # then it takes it to so solution page
-    # The timer has to be put in solution.py. NOT yet working
-    # View currently working for valid inputs
-    sudoku_solution = Solution(sudoku_input).returnGrid()
-    request.session['sudoku_solution'] = repr(sudoku_solution)
+	if sudoku_solution is not None:
+		sudoku_solution = eval(sudoku_solution)
+	else:
+		return HttpResponseRedirect(reverse('solve:no_solution'))
 
-    if not sudoku_solution:
-        return HttpResponseRedirect(reverse('solve:no_solution'))
-    else:
-        return HttpResponseRedirect(reverse('solve:test'))
-
-def test(request):
-    #TODO: this could be used. The other option is to pass it as get parameters
-    sudoku_solution = request.session.get('sudoku_solution', None)
-
-    if sudoku_solution is not None: sudoku_solution = eval(sudoku_solution)
-    else: return HttpResponse("asdasldhl")
-
-    return render(request, 'solve/display.html', {"grid":sudoku_solution})
+	return render(request, 'solve/display.html', {"grid":sudoku_solution})
